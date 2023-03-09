69de7f69f79408e5734598b70729c2b0f08b3333
NowhereCat <gabrielbportela413@gmail.com>
2023-03-09 16:13:20 -0300

    Up

---
+++ my-project/App.js
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react"
+
+export default function App() {
+
+  const [ligar1, setLigar1] = useState(true);
+  const [texto1, setTexto1] = useState('APERTAR');
+
+  const [ligar2, setLigar2] = useState(true);
+  const [texto2, setTexto2] = useState('APERTAR');
+
+
+  const clicarBotao1 = () =>{
+    setLigar1(!ligar1);
+  }
+
+  const clicarBotao2 = () =>{
+    setLigar2(!ligar2);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.view}>
+
+        {
+          ligar1 ?
+            <Text> </Text>
+            :
+            <Text style={styles.text}> Aqui tem um texto que desaparece! </Text>
+        }
+
+        <Button
+          title={texto1}
+          onPress={clicarBotao1}
+          style={styles.button}
+        />
+        
+        {
+          ligar2 ?
+            <Text> </Text>
+            :
+            <Text style={styles.text}> Aqui tem um texto que desaparece! </Text>
+        }
+
+        <Button
+          title={texto2}
+          onPress={clicarBotao2}
+      />
+
+      </View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view:
+  {
+    width: '100%'
+  },
+  button:
+  {
+    paddingLeft: 100
+  },
+  text:
+  {
+    textAlign: 'center'
+  }
+});
